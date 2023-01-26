4fb58b3f24dfbe7d8ec72ee3c4883ad7d5e2ee01
Kaushallrai <kaushallrai03@gmail.com>
2023-01-27 02:43:09 +0545

    fonts/color

--- src/Course_Management_System/Login.java
+++ src/Course_Management_System/Login.java
@@ -35,23 +35,27 @@ public class Login extends javax.swing.JFrame {
         signupPanel = new javax.swing.JPanel();
         dont_have_an_accountLabel = new javax.swing.JLabel();
         signupLabel = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         setMinimumSize(new java.awt.Dimension(1034, 560));
         setUndecorated(true);
-        setPreferredSize(new java.awt.Dimension(1034, 560));
         setResizable(false);
 
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setMinimumSize(new java.awt.Dimension(350, 560));
         jPanel1.setPreferredSize(new java.awt.Dimension(350, 560));
 
+        top_section.setBackground(new java.awt.Color(255, 255, 255));
         top_section.setPreferredSize(new java.awt.Dimension(400, 180));
 
-        Welcome_text.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        Welcome_text.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         Welcome_text.setText("Welcome ");
 
+        loginGoogle.setFont(new java.awt.Font("Arial", 0, 11)); // NOI18N
         loginGoogle.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/icons8_google_16px_1.png"))); // NOI18N
         loginGoogle.setText("Log in with Google");
         loginGoogle.setPreferredSize(new java.awt.Dimension(245, 30));
@@ -84,7 +88,7 @@ public class Login extends javax.swing.JFrame {
             top_sectionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, top_sectionLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(wlvIcon, javax.swing.GroupLayout.DEFAULT_SIZE, 77, Short.MAX_VALUE)
+                .addComponent(wlvIcon, javax.swing.GroupLayout.DEFAULT_SIZE, 80, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(Welcome_text)
                 .addGap(18, 18, 18)
@@ -94,23 +98,28 @@ public class Login extends javax.swing.JFrame {
 
         jPanel1.add(top_section);
 
+        loginEmail.setBackground(new java.awt.Color(255, 255, 255));
         loginEmail.setPreferredSize(new java.awt.Dimension(400, 30));
 
+        jPanel11.setBackground(new java.awt.Color(255, 255, 255));
         jPanel11.setPreferredSize(new java.awt.Dimension(400, 30));
 
-        or_emailLabel.setFont(new java.awt.Font("Dialog", 1, 11)); // NOI18N
-        or_emailLabel.setText("OR LOGIN IN EMAIL");
+        or_emailLabel.setFont(new java.awt.Font("Arial", 1, 11)); // NOI18N
+        or_emailLabel.setText("OR LOGIN WITH EMAIL");
         jPanel11.add(or_emailLabel);
 
         loginEmail.add(jPanel11);
 
         jPanel1.add(loginEmail);
 
+        emailPasswordContainer.setBackground(new java.awt.Color(255, 255, 255));
         emailPasswordContainer.setPreferredSize(new java.awt.Dimension(400, 150));
 
+        emailAddress.setBackground(java.awt.Color.white);
         emailAddress.setPreferredSize(new java.awt.Dimension(245, 50));
         emailAddress.setLayout(new java.awt.BorderLayout(4, 4));
 
+        emailTextFields.setFont(new java.awt.Font("Arial", 0, 11)); // NOI18N
         emailTextFields.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 emailTextFieldsActionPerformed(evt);
@@ -118,23 +127,33 @@ public class Login extends javax.swing.JFrame {
         });
         emailAddress.add(emailTextFields, java.awt.BorderLayout.CENTER);
 
+        emailLabel.setBackground(new java.awt.Color(255, 255, 255));
+        emailLabel.setFont(new java.awt.Font("Arial", 0, 11)); // NOI18N
         emailLabel.setText("Email Address");
         emailAddress.add(emailLabel, java.awt.BorderLayout.PAGE_START);
 
         emailPasswordContainer.add(emailAddress);
 
+        password.setBackground(java.awt.Color.white);
         password.setPreferredSize(new java.awt.Dimension(245, 50));
         password.setLayout(new java.awt.BorderLayout(4, 4));
 
+        passwordLabel.setBackground(new java.awt.Color(255, 255, 255));
+        passwordLabel.setFont(new java.awt.Font("Arial", 0, 11)); // NOI18N
         passwordLabel.setText("Password");
         password.add(passwordLabel, java.awt.BorderLayout.PAGE_START);
+
+        passwordField.setFont(new java.awt.Font("Arial", 0, 11)); // NOI18N
         password.add(passwordField, java.awt.BorderLayout.CENTER);
 
         emailPasswordContainer.add(password);
 
+        keepme_forgot.setBackground(new java.awt.Color(255, 255, 255));
         keepme_forgot.setPreferredSize(new java.awt.Dimension(255, 25));
         keepme_forgot.setLayout(new java.awt.BorderLayout());
 
+        keepmeLabel.setBackground(java.awt.Color.white);
+        keepmeLabel.setFont(new java.awt.Font("Arial", 0, 11)); // NOI18N
         keepmeLabel.setText("Keep me logged in");
         keepmeLabel.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -143,18 +162,21 @@ public class Login extends javax.swing.JFrame {
         });
         keepme_forgot.add(keepmeLabel, java.awt.BorderLayout.WEST);
 
+        forgotLabel.setFont(new java.awt.Font("Arial", 0, 11)); // NOI18N
         forgotLabel.setForeground(new java.awt.Color(255, 51, 51));
-        forgotLabel.setText("Forgot your password?");
-        forgotLabel.setPreferredSize(new java.awt.Dimension(120, 15));
+        forgotLabel.setText("Forgot password");
+        forgotLabel.setPreferredSize(new java.awt.Dimension(90, 15));
         keepme_forgot.add(forgotLabel, java.awt.BorderLayout.LINE_END);
 
         emailPasswordContainer.add(keepme_forgot);
 
         jPanel1.add(emailPasswordContainer);
 
+        login.setBackground(new java.awt.Color(255, 255, 255));
         login.setPreferredSize(new java.awt.Dimension(400, 75));
 
-        loginButton.setText("Login");
+        loginButton.setFont(new java.awt.Font("Arial", 0, 11)); // NOI18N
+        loginButton.setText("Log in ");
         loginButton.setPreferredSize(new java.awt.Dimension(245, 30));
         loginButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -185,16 +207,26 @@ public class Login extends javax.swing.JFrame {
 
         jPanel1.add(login);
 
-        signupPanel.setPreferredSize(new java.awt.Dimension(400, 50));
+        signupPanel.setBackground(new java.awt.Color(255, 255, 255));
+        signupPanel.setMinimumSize(new java.awt.Dimension(400, 60));
+        signupPanel.setPreferredSize(new java.awt.Dimension(400, 60));
 
-        dont_have_an_accountLabel.setText("Don't have an account yet?");
+        dont_have_an_accountLabel.setFont(new java.awt.Font("Arial", 0, 11)); // NOI18N
+        dont_have_an_accountLabel.setText("Don't have an account?");
         signupPanel.add(dont_have_an_accountLabel);
 
-        signupLabel.setForeground(new java.awt.Color(255, 102, 102));
+        signupLabel.setFont(new java.awt.Font("Arial", 0, 11)); // NOI18N
+        signupLabel.setForeground(new java.awt.Color(255, 51, 51));
         signupLabel.setText("Sign up.");
         signupPanel.add(signupLabel);
 
         jPanel1.add(signupPanel);
+
+        jLabel1.setFont(new java.awt.Font("Arial", 0, 8)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("© 2022 ALL RIGHTS RESERVED");
+        jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jPanel1.add(jLabel1);
 
         getContentPane().add(jPanel1, java.awt.BorderLayout.WEST);
 
@@ -237,6 +269,7 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JPanel emailPasswordContainer;
     private javax.swing.JTextField emailTextFields;
     private javax.swing.JLabel forgotLabel;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel11;
